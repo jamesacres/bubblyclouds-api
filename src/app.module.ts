c975cf3a14a25d58cac5ee16159285160b58df60
James Acres <jamesacres@gmail.com>
2024-03-07 14:49:25 +0000

    nest g resource parties

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PartiesModule } from './parties/parties.module';
 
 @Module({
-  imports: [],
+  imports: [PartiesModule],
   controllers: [AppController],
   providers: [AppService],
 })
