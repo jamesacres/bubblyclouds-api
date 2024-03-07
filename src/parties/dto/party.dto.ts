2f087ee49ccd58559c5f074d317dbca2452d8bc3
James Acres <jamesacres@gmail.com>
2024-03-07 18:11:11 +0000

    validate requests

--- src/parties/dto/party.dto.ts
+++ src/parties/dto/party.dto.ts
@@ -1,18 +1,30 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { Transform } from 'class-transformer';
+import { IsDate, IsNotEmpty, IsString } from 'class-validator';
 
 export class PartyDto {
   @ApiProperty()
+  @IsString()
+  @IsNotEmpty()
   partyId: string;
 
   @ApiProperty()
+  @IsString()
+  @IsNotEmpty()
   partyType: string;
 
   @ApiProperty()
+  @IsString()
+  @IsNotEmpty()
   userId: string;
 
   @ApiProperty()
+  @Transform(({ value }) => value && new Date(value))
+  @IsDate()
   expiresAt: Date;
 
   @ApiProperty()
+  @Transform(({ value }) => value && new Date(value))
+  @IsDate()
   createdAt: Date;
 }
